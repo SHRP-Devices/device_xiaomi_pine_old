5b894cc2b7c9d05d9bc9eeda4af479fc03228459
AndroJr7 <56352630+AndroJr7@users.noreply.github.com>
2019-12-24 22:35:28 +0530

    Fix common stuff

--- omni_pine.mk
+++ omni_pine.mk
@@ -19,7 +19,7 @@ $(call inherit-product, vendor/pb/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Omni stuff.
-$(call inherit-product, vendor/pb/config/common.mk)
+$(call inherit-product, vendor/omni/config/common.mk)
 
 # Charger
 PRODUCT_PACKAGES += \
